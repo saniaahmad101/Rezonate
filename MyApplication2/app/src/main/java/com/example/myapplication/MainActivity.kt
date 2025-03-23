1fc39ab97ebca69474b526b2175c2d5ceceb03a4
Sania Ahmad <saniaahmad@Sanias-MacBook-Pro.local>
2025-03-23 10:52:11 -0500

    hi

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener { it:View!
+           val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username : $username and Password : $password")
+
+        }
+
+        }
+    }
---
+++ MyApplication2/signup/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.signup"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.signup"
+        minSdk = 22
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
